4a8f5e8411f4d5502903e0d2077c2457f7a43ab5
Woojin Sim <ssqwe2@gmail.com>
2023-09-26 16:48:17 +0900

    CORS 추가 및 로그기능 추가

--- index.ts
+++ index.ts
@@ -2,10 +2,15 @@ const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const iconv = require("iconv-lite");
+const cors = require("cors");
 
 const app = express();
+const PORT = 4000;
+app.use(cors({ origin: "*" }));
 
 app.get("/API/:element", async (req, res) => {
+  // TODO: 환율 정보 가져온뒤 다시 쏴주는 API 만들어야함!!!
+
   const { element } = req.params;
   if (element) {
     await axios
@@ -13,6 +18,11 @@ app.get("/API/:element", async (req, res) => {
         `https://travel.naver.com/overseas/${element}/country/prepare/check?cardId=entrance`
       )
       .then((response) => {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, "0"); // 시
+        const minutes = now.getMinutes().toString().padStart(2, "0"); // 분
+        const seconds = now.getSeconds().toString().padStart(2, "0"); // 초
+
         const htmlString = response.data;
         // const htmlString = iconv.decode(response.data, "EUC-KR").toString()
         const $ = cheerio.load(htmlString);
@@ -22,6 +32,11 @@ app.get("/API/:element", async (req, res) => {
           ...jsonDataRaw.props.pageProps.pageData[0].geoInfo,
           ...jsonDataRaw.props.pageProps.commonInfo.geoInfo,
         };
+        console.log(
+          `[${hours}:${minutes}:${seconds}][` +
+            (req.ip == "::1" ? "LOCALHOST" : req.ip) +
+            `] ${element}_데이터 요청 (http://localhost:${PORT}/API/${element})`
+        );
         res.json(jsonData);
       });
   } else {
@@ -29,6 +44,6 @@ app.get("/API/:element", async (req, res) => {
   }
 });
 
-app.listen(4000, () => {
-  console.log("서버가 http://localhost:4000 에서 실행중 입니다.");
+app.listen(PORT, () => {
+  console.log(`서버가 http://localhost:${PORT} 에서 실행중 입니다.`);
 });
